da90cef648d67747b276dd3defc87bab3a67c688
Debbie <dbarros64@gmail.com>
2021-03-06 07:01:05 -0800

    Updated shop data with new data. Created a cart utils and added functionality to cart

--- src/Components/Preview-Collection/PreviewCollection.js
+++ src/Components/Preview-Collection/PreviewCollection.js
@@ -9,8 +9,8 @@ const PreviewCollection =({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((_item, index) => index < 4).map(({ id, ...otherItemProps }) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                items.filter((_item, index) => index < 4).map(item => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>

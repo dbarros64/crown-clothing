2b452c126dbcd34591f126f156c4c8527e430ddc
Debbie <dbarros64@gmail.com>
2021-03-06 07:59:09 -0800

    adding cartItem reducer and CartItem dropdown functionality with Redux

--- src/Components/CartDropDown/CartDropDown.js
+++ src/Components/CartDropDown/CartDropDown.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from '../CartItem/CartItem';
 
 import CustomButton from '../Custom-Button/CustomButton';
 import './cart-drop-down.styles.scss';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    cartItems
+});
 
-export default CartDropDown;
+export default connect(mapStateToProps) (CartDropDown);

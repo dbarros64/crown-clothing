0a95d630cdf88fc822da0aa3b586f09cfd47234a
Debbie <dbarros64@gmail.com>
2021-03-07 10:58:19 -0800

    Added a directory reducer and modified shop page and created overview collections component

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import ShopData from  './ShopData';
+import CollectionsOverview from '../../Components/Collections-Overview/Collections-Overview';
 
-import PreviewCollection from '../../Components/Preview-Collection/PreviewCollection';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: ShopData
-        };
-    }
-    render() {
-        const { collections } = this.state;
-        return (
+
+
+const ShopPage = ({ collections }) => (
             <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <PreviewCollection key={id} {...otherCollectionProps} />
-                    ))
-                }
+                <CollectionsOverview />
             </div>
-        )
-    }
-}
+  
+);
 
 
 export default ShopPage;
